2a3336dabde2eac3d036c8f4d2b43373c8617582
F6JO <64877810+F6JO@users.noreply.github.com>
2023-02-05 14:33:11 +0800

    Update BurpExtender.java

--- src/main/java/burp/BurpExtender.java
+++ src/main/java/burp/BurpExtender.java
@@ -60,7 +60,7 @@ public class BurpExtender implements IBurpExtender, IScannerCheck, IContextMenuF
         this.tags = new Tags(callbacks, Config_l);
 //        this.views = Bfunc.Get_Views();
         call.printOutput("@Info: Loading RouteVulScan success");
-        call.printOutput("@Version: RouteVulScan 1.3");
+        call.printOutput("@Version: RouteVulScan 1.4");
         call.printOutput("@From: Code by F6JO");
         call.printOutput("@Github: https://github.com/F6JO/RouteVulScan");
         call.setExtensionName(EXPAND_NAME);
