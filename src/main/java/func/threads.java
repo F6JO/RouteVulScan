36607b519bfc3576caaf94bc2df6d86fc21c15b4
F6JO <64877810+F6JO@users.noreply.github.com>
2023-09-03 22:15:59 +0800

    Update threads.java

--- src/main/java/func/threads.java
+++ src/main/java/func/threads.java
@@ -86,7 +86,12 @@ public class threads implements Task {
             boolean IFconform = true;
 //                if (vul.burp.help.analyzeResponse(newHttpRequestResponse.getResponse()).getStatusCode() == Integer.parseInt(state)) {
 
-            if (states.contains(new Integer(vul.burp.help.analyzeResponse(newHttpRequestResponse.getResponse()).getStatusCode()))) {
+            Integer stat = 0;
+            try {
+                stat = new Integer(vul.burp.help.analyzeResponse(newHttpRequestResponse.getResponse()).getStatusCode());
+            } catch (Exception e) {}
+
+            if (states.contains(stat)) {
                 byte[] resp = newHttpRequestResponse.getResponse();
                 Pattern re_rule = Pattern.compile(re, Pattern.CASE_INSENSITIVE);
                 Matcher pipe = re_rule.matcher(vul.burp.help.bytesToString(resp));
